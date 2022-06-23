b43964c9e51b715baf1bbfbdae9acbcf5dce6a9c
kdutnall <107990921+kdutnall@users.noreply.github.com>
2022-06-23 11:07:42 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+# Generating a matrix to be inverted using the cacheSolve function
 makeCacheMatrix <- function(x = matrix()) {
-
+  # Initialising inv to be "NULL" as a default
+  inv <- NULL
+  
+  # New function on y which generates a variable called x as a copy and puts both x and y in cache
+  set <- function(y){
+    x <<- y
+    inv <<- NULL
+  }
+  
+  get <- function()x
+  
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv 
+  
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
 
+
+
+# Do we already have the matrix held cache?
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+  ## Set inv to be a matrix of the matrix x
+  # If inv does exist then return the cached matrix, display it, and exit out of the function
+  
+  inv <- x$getInverse()
+  if(!is.null(inv)){
+    message("Exisiting Cached Data")
+    return(inv)
+  }
+###############################################################################  
+  # BUT ...
+  # If the inverse matrix does not exist or match the entered one then create it
+  # "solve" will return the direct inverse of the given matrix
+  # Setting x to be the "new" inversed matrix
+  # Return the inverted matrix
+  
+  mat <- x$get()
+  inv <- solve(mat,...)
+  x$setInverse(inv)
+  inv
 }
